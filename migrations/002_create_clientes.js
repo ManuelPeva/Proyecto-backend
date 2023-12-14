50d9a9be628bbee8d5c9b763196f98c1aedafb2d
manuel_859 <manuel_859@outlook.es>
2023-12-14 17:52:52 -0600

    Creación de migration clientes

---
+++ migrations/002_create_clientes.js
@@ -0,0 +1,18 @@
+// migrations/002_create_clientes.js
+exports.up = function (knex) {
+    return knex.schema.createTable('clientes', function (table) {
+      table.increments('ID_Cliente').primary();
+      table.string('Nombre');
+      table.string('Apellido');
+      table.string('Email');
+      table.string('Telefono');
+      table.string('Calle');
+      table.string('Codigo_Postal');
+      table.string('Barrio');
+      table.string('Ciudad');
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable('clientes');
+  };
