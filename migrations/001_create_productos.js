fed93361e0b2fbca520626f611532cae5cb2eb75
manuel_859 <manuel_859@outlook.es>
2023-12-14 17:38:19 -0600

    Corrección del codigo

--- migrations/001_create_productos.js
+++ migrations/001_create_productos.js
@@ -1,9 +1,14 @@
-//migracion
-exports.up = function (knex){
-    return knex.schema.createTable('productos',function(table) {
-        tabla.increments(ID_Producto).primary();
-        table.string('Nombre');
-        tbla.text('Description');
-        tabla.string('SKU').unique();
+// migrations/001_create_productos.js
+exports.up = function (knex) {
+    return knex.schema.createTable('productos', function (table) {
+      table.increments('ID_Producto').primary(); // Corregir esta línea
+      table.string('Nombre');
+      table.text('Descripcion');
+      table.string('SKU').unique();
     });
-};
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable('productos');
+  };
+  
